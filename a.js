dd99f478f4816426111ee9720580709f35cfec1e
kian kheirani <kiankheirani@gmail.com>
2023-07-03 11:59:16 +0430

    'add-pricing'

--- a.js
+++ a.js
@@ -1,11 +1,35 @@
 "use strict";
 const allimage = document.querySelectorAll(".image-elemnt");
-console.log(allimage);
+
 const mainpic = document.querySelector(".main-pic-img");
 const deleteit = document.querySelector(".delete");
 
 const slide = document.querySelectorAll(".slide");
 const slider = document.querySelector(".slider");
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const add_cart = document.querySelector(".add_cart ");
+const price_tag = document.querySelector(".price_tag ");
+const nextt = document.querySelector(".nextt ");
+let b = 0;
+let finalcost = 0;
+plus.addEventListener("click", function () {
+  b++;
+  finalcost = `${125 * b}$ `;
+  price_tag.textContent = finalcost;
+});
+minus.addEventListener("click", function () {
+  if (b > 0) {
+    b--;
+    finalcost = `${125 * b}$ `;
+    price_tag.textContent = finalcost;
+  } else {
+    return;
+  }
+});
+add_cart.addEventListener("click", function () {
+  nextt.textContent = b;
+});
 
 allimage.forEach((a) => {
   a.addEventListener("click", function () {
