6a40358522c07690638c99d5a2e6389d087a14a1
kian kheirani <kiankheirani@gmail.com>
2023-07-03 11:24:34 +0430

    'add'slider'

--- a.js
+++ a.js
@@ -1,11 +1,60 @@
+"use strict";
 const allimage = document.querySelectorAll(".image-elemnt");
-
+console.log(allimage);
 const mainpic = document.querySelector(".main-pic-img");
+const deleteit = document.querySelector(".delete");
+
+const slide = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
 
 allimage.forEach((a) => {
-  a.addEventListener("click", function (e) {
+  a.addEventListener("click", function () {
     const id = this.getAttribute("src");
-    console.log("1");
-    mainpic.setAttribute("src", id);
+    const a = id.split("-");
+
+    const d = a.slice(0, 3).join("-");
+
+    mainpic.setAttribute("src", `${d}.jpg`);
   });
+});
+
+slide.forEach(function (a, b) {
+  a.style.transform = `translateX(${b * 100}%)`;
+});
+const max = slide.length;
+const right = document.querySelector(".slider__btn-r");
+const left = document.querySelector(".slider__btn-l");
+let cur = 0;
+left.addEventListener("click", function () {
+  if (cur == 0) {
+    cur = max - 1;
+  } else {
+    cur--;
+  }
+
+  slide.forEach(function (a, b) {
+    a.style.transform = `translateX(${(b - cur) * 100}%)`;
+  });
+});
+
+right.addEventListener("click", function () {
+  if (cur === max - 1) {
+    cur = 0;
+  } else {
+    cur++;
+  }
+
+  slide.forEach(function (a, b) {
+    a.style.transform = `translateX(${(b - cur) * 100}%)`;
+  });
+});
+const section = document.querySelector(".section");
+const mainpi = document.querySelector(".main-pic-img");
+mainpi.addEventListener("click", function () {
+  section.style.filter = "blur(10px)";
+  slider.classList.remove("hidden");
+});
+deleteit.addEventListener("click", function () {
+  slider.classList.add("hidden");
+  section.style.filter = "none";
 });
